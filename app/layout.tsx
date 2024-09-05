4e928f12b53b010573b92fc54b4801c891679f75
KaliBlip <69836195+KaliBlip@users.noreply.github.com>
2024-09-05 15:44:55 +0000

    changed title from Horizon to Absa-Capital

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,8 +12,8 @@ const ibmPlexSerif = IBM_Plex_Serif ({
 })
 
 export const metadata: Metadata = {
-  title: "Horizon",
-  description: "Horizon is a modern banking platform for everyone.",
+  title: "Absa-Capital",
+  description: "Absa-Capital is a modern banking platform for everyone.",
   icons: {
     icon: '/icons/logo.svg'
   }
