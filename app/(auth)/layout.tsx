7689e1ba26f91d439c0b3a1eea029d387bcbfdac
KaliBlip <69836195+KaliBlip@users.noreply.github.com>
2024-09-05 16:15:34 +0000

    auth image updated

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -11,7 +11,7 @@ export default function RootLayout({
             <div className="auth-asset">
                 <div>
                     <Image 
-                    src="/icons/auth-image.svg"
+                    src="/icons/auth-image.jpg"
                     alt="Auth image"
                     width={500}
                     height={500}
